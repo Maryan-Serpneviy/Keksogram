fc94594927c79f58145c32b77e5d9374fa89a2f4
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-08-13 10:44:51 +0300

    implemented filters change

--- js/filters.js
+++ js/filters.js
@@ -2,25 +2,54 @@
 
 (function() {
     var scaleLine = imgUpload.querySelector('.scale__line');
-    var scaleLevel = imgUpload.querySelector('.scale__level');
     var scalePin = imgUpload.querySelector('.scale__pin');
     var scaleValue = imgUpload.querySelector('.scale__value');
 
     var getSaturationLevel = function() {
         var SCALE_LENGTH = 450;
         var scalePosition = scalePin.offsetLeft;
-        var saturationLevel = scalePosition / SCALE_LENGTH * 100;
+        var saturationLevel = Math.round(scalePosition / SCALE_LENGTH * 100);
         scaleValue.value = saturationLevel;
+        //console.log(scaleValue.value);
 
         return saturationLevel;
     };
 
-    scalePin.addEventListener('mouseup', getSaturationLevel);
+    scalePin.addEventListener('mouseup', onEffectChange);
+    scaleLine.addEventListener('click', onEffectChange);
 
-    var effects = Array.from(imgUpload.querySelectorAll('.effects__item'));
-    effects.forEach(function(elem) {
-        elem.addEventListener('click', function() {
-            scaleValue.value = 0;
-        });
-    });
+    var imgUploadPreview = imgUpload.querySelector('.img-upload__preview img');
+    var effectsList = imgUpload.querySelector('.effects__list');
+
+    var changeFilter = function(effect) {
+        var EFFECTS_MULTIPLIER = {
+            marvin: 100,
+            phobos: 3,
+            heat: 3
+        };
+        var effectSaturation = '';
+        if (effect === 'chrome') {
+            effectSaturation = 'grayscale(' + getSaturationLevel() + ')';
+        } else if (effect === 'sepia') {
+            effectSaturation = 'sepia(' + getSaturationLevel() + ')';
+        } else if (effect === 'marvin') {
+            effectSaturation = 'invert(' + (getSaturationLevel() * EFFECTS_MULTIPLIER.marvin) + '%)';
+        } else if (effect === 'phobos') {
+            effectSaturation = 'blur(' + getSaturationLevel() /* * EFFECTS_MAX.phobos)*/ + 'px)';
+        } else if (effect === 'heat') {
+            effectSaturation = 'brightness(' + getSaturationLevel() /* * EFFECTS_MAX.heat)*/ + ')';
+        }
+        return effectSaturation;      
+    };
+
+    var onEffectChange = function(evt) {
+        var effect = evt.target.value;
+        scaleValue.value = 0;
+        //console.log(scaleValue.value);
+        var filter = changeFilter(effect);
+        
+        imgUploadPreview.style.filter = filter;
+    };
+
+    effectsList.addEventListener('change', onEffectChange);
 })();
