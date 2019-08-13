dfcbd8413bb916da9ef2962ead4be6313e76d266
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-08-13 12:52:22 +0300

    1. filters now work properly. 2. fixed 'change' event

--- js/filters-slider.js
+++ js/filters-slider.js
@@ -1,55 +1,58 @@
-window.imgUpload.classList.remove('hidden');
+'use strict';
 
-var scale = imgUpload.querySelector('.scale');
-var scaleLine = imgUpload.querySelector('.scale__line');
-var scalePin = imgUpload.querySelector('.scale__pin');
-var scaleLevel = imgUpload.querySelector('.scale__level');
+(function() {
+    window.imgUpload.classList.remove('hidden');
 
-var getPinPosition = function(coord, shift) {
-    var SCALE_OVERFLOW = 45;
-    var SCALE_LIMITS = {
-        min: 0,
-        max: scale.offsetWidth - SCALE_OVERFLOW
-    };
-    var pinPosition = '';
-    if (coord > scaleLine.getBoundingClientRect().right) {
-        pinPosition = SCALE_LIMITS.max + 'px';
-    } else if (coord < scaleLine.getBoundingClientRect().left) {
-        pinPosition = SCALE_LIMITS.min + 'px';
-    } else {
-        pinPosition = (scalePin.offsetLeft - shift) + 'px';
-    }
-    return pinPosition;
-};
-
-scalePin.addEventListener('mousedown', function(evt) {
-    evt.preventDefault();
-    var coord = evt.clientX;
-    
-    var onMouseMove = function(moveEvt) {
-        moveEvt.preventDefault();
-        var shift = coord - moveEvt.clientX;
-        coord = moveEvt.clientX;
-
-        var pinPosition = getPinPosition(coord, shift);
-        scalePin.style.left = pinPosition;
-        scaleLevel.style.width = pinPosition;
-        window.pinPosition = pinPosition;
+    var getPinPosition = function(coord, shift) {
+        var SCALE_OVERFLOW = 45;
+        var SCALE_LIMITS = {
+            min: 0,
+            max: scale.offsetWidth - SCALE_OVERFLOW
+        };
+        var pinPosition = '';
+        if (coord > scaleLine.getBoundingClientRect().right) {
+            pinPosition = SCALE_LIMITS.max + 'px';
+        } else if (coord < scaleLine.getBoundingClientRect().left) {
+            pinPosition = SCALE_LIMITS.min + 'px';
+        } else {
+            pinPosition = (scalePin.offsetLeft - shift) + 'px';
+        }
+        return pinPosition;
     };
 
-    var onMouseUp = function(upEvt) {
-        upEvt.preventDefault();
-        scalePin.style.left = window.pinPosition;
-        scaleLevel.style.width = window.pinPosition;
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
-    };
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-});
+    scalePin.addEventListener('mousedown', function(evt) {
+        evt.preventDefault();
+        var coord = evt.clientX;
+        
+        var onMouseMove = function(moveEvt) {
+            moveEvt.preventDefault();
+            var shift = coord - moveEvt.clientX;
+            coord = moveEvt.clientX;
 
-scaleLine.addEventListener('mouseup', function(evt) {
-    evt.preventDefault();
-    scalePin.style.left = evt.offsetX + 'px';
-    scaleLevel.style.width = evt.offsetX + 'px';
-});
+            var pinPosition = getPinPosition(coord, shift);
+            scalePin.style.left = pinPosition;
+            scaleLevel.style.width = pinPosition;
+            window.pinPosition = pinPosition;
+            
+            // dynamic change of effect saturation
+            var effectName = imgUploadPreview.style.filter.substr(0, 2);
+            window.setFilterValue(effectName, getSaturation);
+        };
+
+        var onMouseUp = function(upEvt) {
+            upEvt.preventDefault();
+            scalePin.style.left = pinPosition;
+            scaleLevel.style.width = pinPosition;
+            document.removeEventListener('mousemove', onMouseMove);
+            document.removeEventListener('mouseup', onMouseUp);
+        };
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+    });
+
+    scaleLine.addEventListener('mouseup', function(evt) {
+        evt.preventDefault();
+        scalePin.style.left = evt.offsetX + 'px';
+        scaleLevel.style.width = evt.offsetX + 'px';
+    });
+})();
--- js/filters.js
+++ js/filters.js
@@ -1,54 +1,77 @@
 'use strict';
 
 (function() {
-    var scaleLine = imgUpload.querySelector('.scale__line');
-    var scalePin = imgUpload.querySelector('.scale__pin');
-    var scaleValue = imgUpload.querySelector('.scale__value');
+    window.scale = imgUpload.querySelector('.scale');
+    window.scaleValue = imgUpload.querySelector('.scale__value');
+    window.scaleLine = imgUpload.querySelector('.scale__line');
+    window.scalePin = imgUpload.querySelector('.scale__pin');
+    window.scaleLevel = imgUpload.querySelector('.scale__level');
 
-    var getSaturationLevel = function() {
+    window.getSaturation = function() {
         var SCALE_LENGTH = 450;
         var scalePosition = scalePin.offsetLeft;
-        var saturationLevel = Math.round(scalePosition / SCALE_LENGTH * 100);
-        scaleValue.value = saturationLevel;
+        var saturation = Math.round(scalePosition / SCALE_LENGTH * 100);
+        scaleValue.value = saturation;
         //console.log(scaleValue.value);
 
-        return saturationLevel;
+        return saturation;
     };
 
     scalePin.addEventListener('mouseup', onEffectChange);
     scaleLine.addEventListener('click', onEffectChange);
 
-    var imgUploadPreview = imgUpload.querySelector('.img-upload__preview img');
+    window.imgUploadPreview = imgUpload.querySelector('.img-upload__preview img');
     var effectsList = imgUpload.querySelector('.effects__list');
 
-    var changeFilter = function(effect) {
-        var EFFECTS_MULTIPLIER = {
-            marvin: 100,
-            phobos: 3,
-            heat: 3
-        };
+    // original photo (no effect) is selected by default
+    // this is needed for 'change' event to work properly
+    imgUpload.querySelector('.effects__radio').checked = true;
+
+    var FILTERS_COEF = {
+        chrome: 0.01,
+        sepia: 0.01,
+        marvin: 1,
+        phobos: 0.15,
+        heat: 0.05
+    };
+
+    var changeFilter = function(effect, saturation) {
         var effectSaturation = '';
         if (effect === 'chrome') {
-            effectSaturation = 'grayscale(' + getSaturationLevel() + ')';
+            effectSaturation = 'grayscale(' + saturation() + ')';
         } else if (effect === 'sepia') {
-            effectSaturation = 'sepia(' + getSaturationLevel() + ')';
+            effectSaturation = 'sepia(' + saturation() + ')';
         } else if (effect === 'marvin') {
-            effectSaturation = 'invert(' + (getSaturationLevel() * EFFECTS_MULTIPLIER.marvin) + '%)';
+            effectSaturation = 'invert(' + (saturation() * FILTERS_COEF.marvin) + '%)';
         } else if (effect === 'phobos') {
-            effectSaturation = 'blur(' + getSaturationLevel() /* * EFFECTS_MAX.phobos)*/ + 'px)';
+            effectSaturation = 'blur(' + saturation() /* * EFFECTS_MAX.phobos)*/ + 'px)';
         } else if (effect === 'heat') {
-            effectSaturation = 'brightness(' + getSaturationLevel() /* * EFFECTS_MAX.heat)*/ + ')';
+            effectSaturation = 'brightness(' + saturation() /* * EFFECTS_MAX.heat)*/ + ')';
         }
         return effectSaturation;      
+    };
+
+    window.setFilterValue = function(effect, saturation) {
+        this.console.log(effect);
+
+        if (effect === 'gr') {
+            window.imgUploadPreview.style.filter = 'grayscale(' + saturation() * FILTERS_COEF.chrome + ')';
+        } else if (effect === 'se') {
+            window.imgUploadPreview.style.filter = 'sepia(' + saturation() * FILTERS_COEF.sepia + ')';
+        } else if (effect === 'in') {
+            window.imgUploadPreview.style.filter = 'invert(' + (saturation() * FILTERS_COEF.marvin) + '%)';
+        } else if (effect === 'bl') {
+            window.imgUploadPreview.style.filter = 'blur(' + saturation() * FILTERS_COEF.phobos + 'px)';
+        } else if (effect === 'br') {
+            window.imgUploadPreview.style.filter = 'brightness(' + saturation() * FILTERS_COEF.heat + ')';
+        }    
     };
 
     var onEffectChange = function(evt) {
         var effect = evt.target.value;
         scaleValue.value = 0;
-        //console.log(scaleValue.value);
-        var filter = changeFilter(effect);
-        
-        imgUploadPreview.style.filter = filter;
+        var filter = changeFilter(effect, window.getSaturation);
+        window.imgUploadPreview.style.filter = filter;
     };
 
     effectsList.addEventListener('change', onEffectChange);
