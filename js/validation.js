a7a372db79efdb79d7f07c1beda342315e06974e
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-09-20 10:57:11 +0300

    Validation fixed and upgraded

--- js/validation.js
+++ js/validation.js
@@ -1,33 +1,45 @@
-const MAX_HASHTAGS = 5;
-const MAX_HASHTAG_LENGTH = 20;
+import Const from './constants.js';
+
 const userHashtags = document.querySelector('.text__hashtags');
 
-const validateHashtags = arr => {
-    let arrLowerCase = [];
-    for (let i = 0; i < arr.length; i++) {
-        arrLowerCase[i] = arr[i].toLowerCase();
+const validateHashtags = hashtagsArr => {
+    const lowerCaseHashtags = hashtagsArr.map(element => element.toLowerCase());
+    const uniqueHashtags = [...new Set(lowerCaseHashtags)];
+
+    const isInvalid = () => {
+        userHashtags.style.border = `2px solid ${Const.COLOR.DANGER}`;
+        userHashtags.style.outline = 'none';
     }
     userHashtags.setCustomValidity('');
-    if (arr.length > MAX_HASHTAGS) {
-        userHashtags.setCustomValidity(`Maximum of hashtags is: ${MAX_HASHTAGS}`);
-    }
-    if (!checkUniqueValues(arrLowerCase)) {
-        userHashtags.setCustomValidity(`Same hashtag cannot be used twice (tags are not case sensitive).`);
-    }
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === '#') {
+    hashtagsArr.forEach(element => {
+        if (element === '#') {
             userHashtags.setCustomValidity(`Hashtag cannot consist of just a lattice. Delete the extra character or add it.`);
-        } else if (arr[i].charAt(0) !== '#') {
-            userHashtags.setCustomValidity(`Hashtag ${arr[i]} must begin with "#".`);
-        } else if (arr[i].slice(1).indexOf('#') !== -1) {
-            userHashtags.setCustomValidity(`Hashtags ${arr[i]} must be separated by a space.`);
-        } else if (arr[i].length > MAX_HASHTAG_LENGTH) {
-            userHashtags.setCustomValidity(`The maximum length of one hashtag is 20 characters, including "#". Shorten your hashtag ${arr[i]}.`);
+            isInvalid();
+        } else if (element.charAt(0) !== '#') {
+            userHashtags.setCustomValidity(`Hashtag ${element} must begin with "#".`);
+            isInvalid();
+        } else if (element.slice(1).indexOf('#') !== -1) {
+            userHashtags.setCustomValidity(`Hashtags ${element} must be separated by white space.`);
+            isInvalid();
+        } else if (element.length > Const.HASHTAG.MAX_LENGTH) {
+            userHashtags.setCustomValidity(`Maximum hashtag length of ${Const.HASHTAG.MAX_LENGTH} is exceeded. Shorten this hashtag: ${element}.`);
+            isInvalid();
         }
+    });
+    if (hashtagsArr.length > Const.HASHTAG.MAX_AMOUNT) {
+        userHashtags.setCustomValidity(`Maximum hashtags amount of ${Const.HASHTAG.MAX_AMOUNT} is exceeded`);
+        isInvalid();
+    }
+    if (hashtagsArr.length !== uniqueHashtags.length) {
+        userHashtags.setCustomValidity(`Same hashtag cannot be used twice (case is ignored).`);
+        isInvalid();
     }
 };
 
 userHashtags.addEventListener ('input', () => {
     const hashtagsArr = userHashtags.value.split(' ');
+
+    userHashtags.style.border = 'none';
+    userHashtags.style.outline = '5px auto -webkit-focus-ring-color';
     validateHashtags (hashtagsArr);
 });
