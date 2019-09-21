caf31f6f0887c686f46390f140ef5c38afa3beb3
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-09-21 12:21:03 +0300

    Upgraded getRandom function

--- js/utils.js
+++ js/utils.js
@@ -4,11 +4,28 @@ export default {
      * Shuffles array in place. ES6 version
      * @param {Array} a items An array containing the items.
      */
-    shuffle: function(a) {
-        for (let i = a.length - 1; i > 0; i--) {
+    shuffle (arr) {
+        for (let i = arr.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            [a[i], a[j]] = [a[j], a[i]];
+            [arr[i], arr[j]] = [arr[j], arr[i]];
         }
-        return a;
+        return arr;
+    },
+    getRandomUniqueArrayElements (sourceArray, amount) {
+        const shuffle = arrayToShuffle => {
+            const shuffledArray = Array.from(arrayToShuffle);
+            for (let i = shuffledArray.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+            }
+            return shuffledArray;
+        };
+        const uniquesArray = [...new Set(sourceArray)];
+        const randomsArray = shuffle(uniquesArray);
+        let limitedArray = [];
+        for (let i = 0; i < amount; i++) {
+            limitedArray.push(randomsArray[i]);
+        }
+        return limitedArray;
     }
 }
