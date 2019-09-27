ba7e3b4e6e92068be07297ea57420cd8789ccc7d
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-09-27 20:34:16 +0300

    Bind used

--- js/filters.js
+++ js/filters.js
@@ -20,30 +20,32 @@ const effectsList = imgUpload.querySelector('.effects__list');
 // this is needed for 'change' event to work properly
 imgUpload.querySelector('.effects__radio').checked = true;
 
-const setEffectSaturation = (effect, saturation) => {
+const setEffectSaturation = function(effect, saturation) {
     if (effect === 'chrome' || effect === 'gr') {
-        imgUploadPreview.style.filter = `grayscale(${saturation() * Const.FILTERS_COEF.CHROME})`;
+        this.style.filter = `grayscale(${saturation() * Const.FILTERS_COEF.CHROME})`;
     } else if (effect === 'sepia' || effect === 'se') {
-        imgUploadPreview.style.filter = `sepia(${saturation() * Const.FILTERS_COEF.SEPIA})`;
+        this.style.filter = `sepia(${saturation() * Const.FILTERS_COEF.SEPIA})`;
     } else if (effect === 'marvin' || effect === 'in') {
-        imgUploadPreview.style.filter = `invert(${saturation() * Const.FILTERS_COEF.MARVIN}%)`;
+        this.style.filter = `invert(${saturation() * Const.FILTERS_COEF.MARVIN}%)`;
     } else if (effect === 'phobos' || effect === 'bl') {
-        imgUploadPreview.style.filter = `blur(${saturation() * Const.FILTERS_COEF.PHOBOS}px)`;
+        this.style.filter = `blur(${saturation() * Const.FILTERS_COEF.PHOBOS}px)`;
     } else if (effect === 'heat' || effect === 'br') {
-        imgUploadPreview.style.filter = `brightness(${saturation() * Const.FILTERS_COEF.HEAT})`;
+        this.style.filter = `brightness(${saturation() * Const.FILTERS_COEF.HEAT})`;
     } else {
-        imgUploadPreview.style.filter = `none`;
+        this.style.filter = `none`;
     }
 };
 
+const setFilter = setEffectSaturation.bind(imgUploadPreview);
+
 const dynamicSaturation = () => {
     const effectName = imgUploadPreview.style.filter.substr(0, 2);
-    setEffectSaturation(effectName, getSaturation);
+    setFilter(effectName, getSaturation);
 };
 
 const onEffectChange = evt => {
     const effectName = evt.target.value;
-    setEffectSaturation(effectName, getSaturation);
+    setFilter(effectName, getSaturation);
 };
 
 scaleLine.addEventListener('click', dynamicSaturation);
--- js/validation.js
+++ js/validation.js
@@ -2,44 +2,45 @@ import Const from './constants.js';
 
 const userHashtags = document.querySelector('.text__hashtags');
 
-const validateHashtags = hashtagsArr => {
+const validateHashtags = function(hashtagsArr) {
+    console.log(this);
     const lowerCaseHashtags = hashtagsArr.map(element => element.toLowerCase());
     const uniqueHashtags = [...new Set(lowerCaseHashtags)];
 
     const isInvalid = () => {
-        userHashtags.style.border = `2px solid ${Const.COLOR.DANGER}`;
-        userHashtags.style.outline = 'none';
+        this.style.border = `2px solid ${Const.COLOR.DANGER}`;
+        this.style.outline = 'none';
     };
-    userHashtags.setCustomValidity('');
-    hashtagsArr.forEach(element => {
+    this.setCustomValidity('');
+    this.forEach(element => {
         if (element === '#') {
-            userHashtags.setCustomValidity(`Hashtag cannot consist of just a lattice. Delete the extra character or add it.`);
+            this.setCustomValidity(`Hashtag cannot consist of just a lattice. Delete the extra character or add it.`);
             isInvalid();
         } else if (element.charAt(0) !== '#') {
-            userHashtags.setCustomValidity(`Hashtag ${element} must begin with "#".`);
+            this.setCustomValidity(`Hashtag ${element} must begin with "#".`);
             isInvalid();
         } else if (element.slice(1).indexOf('#') !== -1) {
-            userHashtags.setCustomValidity(`Hashtags ${element} must be separated by white space.`);
+            this.setCustomValidity(`Hashtags ${element} must be separated by white space.`);
             isInvalid();
         } else if (element.length > Const.HASHTAG.MAX_LENGTH) {
-            userHashtags.setCustomValidity(`Maximum hashtag length of ${Const.HASHTAG.MAX_LENGTH} is exceeded. Shorten this hashtag: ${element}.`);
+            this.setCustomValidity(`Maximum hashtag length of ${Const.HASHTAG.MAX_LENGTH} is exceeded. Shorten this hashtag: ${element}.`);
             isInvalid();
         }
     });
     if (hashtagsArr.length > Const.HASHTAG.MAX_AMOUNT) {
-        userHashtags.setCustomValidity(`Maximum hashtags amount of ${Const.HASHTAG.MAX_AMOUNT} is exceeded`);
+        this.setCustomValidity(`Maximum hashtags amount of ${Const.HASHTAG.MAX_AMOUNT} is exceeded`);
         isInvalid();
     }
     if (hashtagsArr.length !== uniqueHashtags.length) {
-        userHashtags.setCustomValidity(`Same hashtag cannot be used twice (case is ignored).`);
+        this.setCustomValidity(`Same hashtag cannot be used twice (case is ignored).`);
         isInvalid();
     }
 };
 
-userHashtags.addEventListener('input', () => {
-    const hashtagsArr = userHashtags.value.split(' ');
+const validate = validateHashtags.bind(userHashtags);
 
-    userHashtags.style.border = 'none';
-    userHashtags.style.outline = '5px auto -webkit-focus-ring-color';
-    validateHashtags(hashtagsArr);
+userHashtags.addEventListener('input', function() {
+    this.style.border = 'none';
+    this.style.outline = '5px auto -webkit-focus-ring-color';
+    validate(this.value.split(' '));
 });
