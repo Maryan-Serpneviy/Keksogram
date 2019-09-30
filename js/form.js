bc5e6ff06ad7bed4f584d4403153eaa7059b14e4
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-09-30 19:14:26 +0300

    Form upload logic fixed

--- js/form.js
+++ js/form.js
@@ -1,14 +1,25 @@
 import AJAX from './ajax.js';
+import Picture from './picture.js';
+import render from './render.js';
 
-const imgUpload = document.querySelector('.img-upload');
-const form = imgUpload.querySelector('.img-upload__form');
+const picturesContainer = document.querySelector('.pictures');
+const imgUpload = document.querySelector('.img-upload__overlay');
+const form = document.querySelector('.img-upload__form');
+const textHashtags = imgUpload.querySelector('.text__hashtags');
+const textDescription = imgUpload.querySelector('.text__description');
+const submit = imgUpload.querySelector('.img-upload__submit');
 
 const successHandler = () => {
     imgUpload.classList.add('hidden');
     form.reset();
 };
 
-form.addEventListener('submit', function(evt) {
+submit.addEventListener('click', function(evt) {
     evt.preventDefault();
-    AJAX.save(new FormData(this), successHandler, AJAX.statusHandler, 'uploaded');
+
+    const pictureUrl = document.querySelector('.img__preview').src;
+    const pictureDescription = `${textHashtags.value} ${textDescription.value}`;
+    const picture = new Picture({ url: pictureUrl, likes: 0, comments: '', description: pictureDescription });
+    picturesContainer.appendChild(render(picture));
+    AJAX.save(new FormData(form), successHandler, AJAX.statusHandler, 'uploaded');
 });
