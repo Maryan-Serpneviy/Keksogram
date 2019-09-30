f9c3a056c95323bbc53881ec6665f6bfeeefdc41
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-09-30 19:05:28 +0300

    Naming fix

--- js/render.js
+++ js/render.js
@@ -1,9 +1,9 @@
 const pictureTemplate = document.querySelector('#picture-template').content;
 
 export default data => {
-    const thumbElement = pictureTemplate.querySelector('.picture__link').cloneNode(true);
-    thumbElement.querySelector('.picture__img').src = data.url;
-    thumbElement.querySelector('.picture__stat--likes').textContent = data.likes;
-    thumbElement.querySelector('.picture__stat--comments').textContent = data.comments.length;
-    return thumbElement;
+    const picture = pictureTemplate.querySelector('.picture__link').cloneNode(true);
+    picture.querySelector('.picture__img').src = data.url;
+    picture.querySelector('.picture__stat--likes').textContent = data.likes;
+    picture.querySelector('.picture__stat--comments').textContent = data.comments.length;
+    return picture;
 };
